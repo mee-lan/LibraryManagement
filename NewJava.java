dce8d34dfcedc6957e5ee26440007e2fc6d6d342
mee-lan <milan.gyawali404@gmail.com>
2023-03-09 12:47:09 +0545

    Added have a nice day

--- NewJava.java
+++ NewJava.java
@@ -2,5 +2,6 @@ class NewJava{
     public static void main(String[] args) {
         System.out.println("Im a new java file");
         System.out.println("hellooooo...");
+        System.out.println("Have a nice day...");
     }
 }
